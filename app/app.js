7d3b5674e3f1841f39a4c6f5c673eb954ab149be
Catherine Lin <catherine.lin.93@gmail.com>
2018-05-20 11:03:36 -0700

    added playerTurn property

--- app/app.js
+++ app/app.js
@@ -4,53 +4,106 @@ $(document).ready(function() {
         start: false,
         strict: false,
         count: 0,
-        possibilities: ['#b1', '#b2', '#b3', '#b4'],
+        possibilities: ['b1', 'b2', 'b3', 'b4'],
         currentGame: [],
-        player: []
+        player: [],
+        playerTurn: false
     }
 
+    // start the game
     function startGame() {
         clearGame();
     }
 
+    // initialize the variables
     function clearGame() {
         game.currentGame = [];
         game.count = 0;
         addCount();
     }
 
+    // count the number of moves
     function addCount() {
         game.count++;
         $('#count').text(game.count);
         newMove();
     }
 
+    // randomly selects a id from the possibilities array
     function newMove() {
         game.currentGame.push(game.possibilities[Math.floor(Math.random()*4)]);
+        console.log('current game: ' + game.currentGame);
         showMoves();
+        game.playerTurn = true;
     }
 
+    // show the next move on the UI
     function showMoves() {
+        game.playerTurn = false;
         var i = 0;
         var moves = setInterval(() => {
-            playGame(game.currentGame[i]);i
+            playGame(game.currentGame[i]);
             i++;
             if (i >= game.currentGame.length) {
                 clearInterval(moves);
             }
         }, 600);
-
-        
+        clearPlayer();
+        console.log('before change ' + game.playerTurn);
+        game.playerTurn = true;
+        console.log("after change " + game.playerTurn);
     }
 
     function playGame(id) {
-        $(id).addClass('hover')
+        $(`#${id}`).addClass('hover')
         playSound(id);
         setTimeout(() => {
-            $(id).removeClass('hover');
+            $(`#${id}`).removeClass('hover');
         }, 300);
     }
 
+    function clearPlayer() {
+        game.player = [];
+    }
+
+    function playerMoves(id) {
+        console.log('player move ' + game.playerTurn);
+        if (game.playerTurn == true) {
+            game.player.push(id);
+            checkPlayerMove(id);
+        }
+        console.log('player moves: ' + game.player);
+        
+    }
+
+    function checkPlayerMove(id) {
+        // if player makes a mistake
+        if (game.player[game.player.length - 1] !== game.currentGame[game.player.length - 1]) {
+            game.playerTurn = false;
+            if (game.strict) {
+                console.log('you lose, start over');
+                startGame();
+            } else {
+                console.log('wrong, try again');
+                setTimeout(() => {
+                    showMoves();
+                }, 1000);
+                
+            }
+        } else { // if player makes the correct move
+            console.log('correct!');
+            playSound(id);
+            let check = game.player.length === game.currentGame.length;
+            if (check) {
+                setTimeout(() => {
+                    addCount();
+                    clearPlayer();
+                }, 800);
+                
+            }
+            
+        }
+    }
 
     function playSound(id) {
         var a;
@@ -68,12 +121,23 @@ $(document).ready(function() {
                 a = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
                 break;
         }
+        a.play();
     }
 
+    // listens to button clicks
+    $('.buttons').click((e) => {
+        playerMoves(e.target.id);
+    })
 
+    // start the game on click
     $('#start-btn').click(() => {
         console.log('start game');
         startGame();
+    })
+
+    $('#strict-btn').click(() => {
+        game.strict = !game.strict;
+        console.log('strict mode: ' + game.strict);
     })
 
 
